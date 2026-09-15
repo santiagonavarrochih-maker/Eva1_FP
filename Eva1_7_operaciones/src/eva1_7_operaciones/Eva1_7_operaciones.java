d521063669b08ea25fd61e55793ffb35c56d8bad
santiagonavarrochih-maker <santiagonavarrochih@gmail.com>
2026-09-14 18:58:50 -0600

    Add files via upload

---
+++ Eva1_7_operaciones/src/eva1_7_operaciones/Eva1_7_operaciones.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_7_operaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int suma,val1, val2;
+        val1 = 100;
+        val2 = 50;
+        suma = val1 + val2;
+        System.out.println("val1 =");
+        System.out.println(val1);
+        System.out.println("val2 =");
+        System.out.println(val2);
+        System.out.println("suma =");
+        System.out.println(suma);
+        
+        int multi;
+        multi = val1 * val2;
+        System.out.print("Multiplocación =");
+        System.out.println(multi);
+        int divi;
+        divi = val1 / val2;
+        System.out.println("divicion =");
+        System.out.println(divi);
+        val1 = 10;
+        val2 = 3;
+        divi = val1 / val2;
+        System.out.println("val1 =");
+        System.out.println(val1);
+        System.out.println("val1 =");
+        System.out.println(val1);
+        System.out.println("Division  =");
+        System.out.println(divi);
+        double resu;
+        resu = val1 / val2;
+        System.out.println("Division  =");
+        System.out.println(resu);
+        double val2copia;
+        val2copia = 3;
+        resu = val1 / val2copia;
+        System.out.println("resu  =");
+        System.out.println(resu);
+        
+    }
+    
+}
---
+++ eva1_7 operaciones2/src/eva1_7/operaciones2/Eva1_7Operaciones2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7.operaciones2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Eva1_7Operaciones2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int resu, val1 = 10, val2 = 5;
+        resu = 100 + val1 * (val1 / val2);
+        System.out.println("resu  =");
+;        System.out.println(resu);
+
+    }
+    
+}
